9a65165abb8ddaacd149f70c8983dc22eb40f0f0
Andrey Valentynovich <andrey.valentynovich@karuna.group>
2025-01-26 01:24:38 +0300

    list 5 6

---
+++ lesson_4/list_5.py
@@ -0,0 +1,11 @@
+"""
+Дан список
+['samsung', 'lg', 'xerox', 'bosch']
+Удалить элемент с именем 'xerox'
+Добавить элемент на 2 место 'indesit'
+"""
+
+LI_LI = ["samsung", "lg", "xerox", "bosch"]
+LI_LI.remove("xerox")
+LI_LI.insert(1, "indesit")
+print(LI_LI)
