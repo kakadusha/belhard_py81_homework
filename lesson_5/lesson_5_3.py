c38e07763eaaf7f77a5d011eeb876a66d2b913be
Andrey Valentynovich <andrey.valentynovich@karuna.group>
2025-02-04 02:06:29 +0300

    5_3

--- lesson_5/lesson_5_3.py
+++ lesson_5/lesson_5_3.py
@@ -2,6 +2,10 @@
 дан словарь
 d = {'one':11, 'two':22, 'hello':'python', True:False}
 запросить номер элемента и удалить его из словаря с помощью del.
-
 """
 
+d = {"one": 11, "two": 22, "hello": "python", True: False}
+print(d)
+key = input("Введите порядковый номерэлемента для удаления (номера с единицы): ")
+del d[list(d.keys())[int(key) - 1]]
+print(d)
