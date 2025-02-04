728b0df87372f075ee36c296e3e9e58ff3dc661e
Andrey Valentynovich <andrey.valentynovich@karuna.group>
2025-02-04 18:08:14 +0300

    6_6

--- lesson_6/les6_6.py
+++ lesson_6/les6_6.py
@@ -6,3 +6,24 @@
     Пары могут образовать только следующие переменные - a1-a3, a2-a4, a3-a4"
 """
 
+# переменные
+a1 = 1.1
+a2 = 2
+a3 = "3.3"
+a4 = 4
+
+# тесты
+print(
+    "Все переменные дробные числа: ",
+    all(map(lambda x: isinstance(x, float), [a1, a2, a3, a4])),
+)
+
+print(
+    "Одна из переменных это строка: ",
+    any(map(lambda x: isinstance(x, str), [a1, a2, a3, a4])),
+)
+pairs = [(a1, a3), (a2, a4), (a3, a4)]
+print(
+    "Одна пара переменных является целочисленным_и тип_ами: ",
+    any(map(lambda x: isinstance(x[0], int) and isinstance(x[1], int), pairs)),
+)
