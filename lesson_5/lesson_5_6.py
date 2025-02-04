12b2ad2cd9759e637270db2827121903b572e625
Andrey Valentynovich <andrey.valentynovich@karuna.group>
2025-02-04 13:10:18 +0300

    5_6

--- lesson_5/lesson_5_6.py
+++ lesson_5/lesson_5_6.py
@@ -1,4 +1,4 @@
-'''
+"""
 Запросить 3 раза строку из нескольких чисел через пробел
     - вывести все уникальные числа по возрастанию
     - вывести числа которые есть в каждой строке
@@ -11,10 +11,29 @@
     >>> 1 2 22 33
     >>> 1 2 33 44
 
-
     1) 1 2 11 22 33 44
     2) 1 2
-    3) 11 44
-    
-'''
+    3) 11 44 
+"""
 
+input_1 = input("Введите нескольких чисел через пробел, 1: ")
+input_2 = input("Введите нескольких чисел через пробел, 2: ")
+input_3 = input("Введите нескольких чисел через пробел, 3: ")
+
+set_int_1 = set(map(int, input_1.split()))
+set_int_2 = set(map(int, input_2.split()))
+set_int_3 = set(map(int, input_3.split()))
+
+all_set = set_int_1 | set_int_2 | set_int_3
+print("Все уникальные числа по возрастанию: ", sorted(all_set))
+
+common_set = set_int_1 & set_int_2 & set_int_3
+print("Числа что есть в каждой строке: ", common_set)
+
+unique_set = (
+    all_set
+    - (set_int_1 & set_int_2)
+    - (set_int_1 & set_int_3)
+    - (set_int_2 & set_int_3)
+)
+print("Числа что есть только в одной из трех строк: ", unique_set)
